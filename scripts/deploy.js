9a25292dd3e05d6e53b72c5c6ae1295b2d1666fb
vatsrounak <rounakvats01@gmail.com>
2023-07-17 10:28:46 +0530

    new commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,22 +1,24 @@
-const { ethers } = require("hardhat");
+const hre = require("hardhat");
 
 async function main() {
   // Retrieve the contract factories and deployer's address
-  const [deployer] = await ethers.getSigners();
-  const EnergyTradingFactory = await ethers.getContractFactory("EnergyTrading");
+  const [deployer] = await hre.ethers.getSigners();
+  
+  // the below code is used to create an instance of the EnergyTrading. sol smart contract
+  const EnergyTradingFactory = await hre.ethers.getContractFactory("EnergyTrading");
 
   // Deploy the contract
-  console.log("Deploying the EnergyTrading contract...");
-  const energyTrading = await EnergyTradingFactory.deploy();
-  await energyTrading.deployTransaction.wait(); // Wait for the deployment transaction to be mined
+console.log("Deploying the EnergyTrading contract...");
+const energyTrading = await EnergyTradingFactory.deploy();
+await energyTrading.deployed(); // Wait for the deployment transaction to be mined
 
-  console.log("EnergyTrading contract deployed to:", energyTrading.address);
+console.log("EnergyTrading contract deployed to:", energyTrading.address);
 }
 
 // Execute the deployment
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
+.then(() => process.exit(0))
+.catch((error) => {
+  console.error(error);
+  process.exit(1);
 });
